29f349df04ee530e2e778ef826ef0bde14337c2b
Cyril-Cristo-Johns <cyrilcristojohns@gmail.com>
2026-09-21 10:40:32 +0530

    Filter Route

--- controllers/report.controller.js
+++ controllers/report.controller.js
@@ -57,7 +57,43 @@ export const createReport= async (req, res)=>{
 }
 
 export const filterReport= async (req, res)=>{
-        
+    // let {_id}= req.user;
+    
+    
+    try{
+        let { pincode, district, state} = req.body;
+        let parameters={};
+        if(pincode.length)
+            parameters.pincode= pincode;
+        if(district.length)
+            parameters.district= district;
+        if(state.length)
+            parameters.state= state;
+
+        let filtered_report= await Report.find(parameters);
+        if(!filtered_report)
+            res.status(400).json({
+        success: false,
+        error: "Could'nt find any Reports"
+        })
+
+        res.status(200).json(
+            {
+                success: true,
+                filtered_report
+            }
+        )
+    }
+    catch(err){
+        console.log(err.message);
+        res.status(500).json(
+            {
+                success: true,
+                error: "Server Error!: Reports not found"
+            }
+        )
+    }
+
 }
 
 export const singleReport =async (req, res)=>{
